483c574e021d6be91cd910bc573740ce30a11fcd
Mateusz Nawrot <mateusz.nawrot@intive.com>
2017-11-20 20:02:21 +0100

    remove unnecessary methods from SchedulersProvider.kt

--- domain/src/main/java/com/nawrot/mateusz/compass/domain/base/SchedulersProvider.kt
+++ domain/src/main/java/com/nawrot/mateusz/compass/domain/base/SchedulersProvider.kt
@@ -1,16 +1,10 @@
 package com.nawrot.mateusz.compass.domain.base
 
-import io.reactivex.CompletableTransformer
 import io.reactivex.ObservableTransformer
-import io.reactivex.SingleTransformer
 
 
 interface SchedulersProvider {
 
-    fun completableTransformer(): CompletableTransformer
-
     fun <T> observableTransformer(): ObservableTransformer<T, T>
-
-    fun <T> singleTransformer(): SingleTransformer<T, T>
 
 }
